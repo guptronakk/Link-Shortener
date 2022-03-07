74c955013449330f9719e5660d7db13e1e564c7d
apoorvaron <83576954+apoorvaron@users.noreply.github.com>
2022-03-07 19:45:15 +0530

    Add files via upload

---
+++ ls/enviornment.php
@@ -0,0 +1,14 @@
+<?php
+    // $env_server = "sql210.epizy.com";
+    // $env_username = "epiz_31222381";
+    // $env_password = "gT43qQV9Sk8L";
+    // $env_database = "epiz_31222381_db";
+    // $env_port = "3306";
+
+
+    $env_server = "localhost";
+    $env_username = "root";
+    $env_password = "root";
+    $env_database = "dB2";
+    $env_port = "8889";
+?>
---
+++ ls/tiny/siteName.php
@@ -0,0 +1,7 @@
+<?php
+    // $siteLink = "http://localhost:8888/ls/tiny/";
+    // $siteLink = "http://localhost:8888/ls/tiny/";
+    $siteLink = "http://localhost:8888/ls/tiny/";
+    $siteName = "http://localhost:8888/ls/tiny/";
+   
+?>
